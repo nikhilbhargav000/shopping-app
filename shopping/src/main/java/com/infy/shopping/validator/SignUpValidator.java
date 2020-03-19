793b3879998d30f77776858a84a0c6dbd93bc210
Nikhil Bhargav <nikhilbhargav000@gmail.com>
2020-03-19 09:11:22 +0530

    First Cimmit

--- shopping/src/main/java/com/infy/shopping/validator/SignUpValidator.java
+++ shopping/src/main/java/com/infy/shopping/validator/SignUpValidator.java
@@ -1,5 +1,8 @@
 package com.infy.shopping.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -16,7 +19,21 @@ public class SignUpValidator implements Validator<SignUp> {
 		return true;
 	}
 
+	
+	public boolean isValidName(String name) {
+		String regex = "[a-zA-Z]+";
+		if (!Pattern.matches(regex, name)) {
+			throw new SAppsException(HttpStatus.BAD_REQUEST.value(), "Name contains invalid characters");
+		}
+		return true;
+	}
+	
 	public boolean isValidPassword(String password) {
+		String regex = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])";
+		if (!Pattern.matches(regex, password)) {
+			throw new SAppsException(HttpStatus.BAD_REQUEST.value(),
+					"Password should contain atleast a uppercase and a lower case , a number and a special character");
+		}
 		return true;
 	}
 	
@@ -29,7 +46,7 @@ public class SignUpValidator implements Validator<SignUp> {
 		if (StringUtils.isBlank(signUp.getName()) || StringUtils.isBlank(signUp.getEmail()) || 
 				StringUtils.isBlank(signUp.getPassword()) || StringUtils.isBlank(signUp.getConfirmPassword()) || 
 				StringUtils.isBlank(signUp.getAccountType())) {
-			throw new SAppsException(HttpStatus.BAD_REQUEST.value(), "Mandatory fields cannot be empty");
+			throw new SAppsException(HttpStatus.BAD_REQUEST.value(), "All fields are mandatory");
 		}
 		return true;
 	}
